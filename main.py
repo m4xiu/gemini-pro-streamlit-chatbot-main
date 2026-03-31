994a701fc4f73e9812ec6c099b1a2b93cd509fff
Mathew <yogamathuzz@gamil.com>
2026-04-01 02:05:11 +0530

    switched to old

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from google import genai
 import os
-from dotenv import load_dotenv
+
 import streamlit as st
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
 
 
 # Load environment variables
